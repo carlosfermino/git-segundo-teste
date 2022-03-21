7cc14114545036d4a87b1291b9297c97c62a205a
carlosfermino <brutosacf@gmail.com>
2022-03-21 17:49:06 -0300

    Outravez

--- projeto.git/src/main/java/projeto/git/UploadGit.java
+++ projeto.git/src/main/java/projeto/git/UploadGit.java
@@ -5,6 +5,7 @@ public class UploadGit {
 	public static void main(String[] args) {
 		System.out.println("Hello git !!!");
 		System.out.println("Alterei o projeto git outraves");
+		System.out.println("Vou alterar outra vez");
 
 	}
 
